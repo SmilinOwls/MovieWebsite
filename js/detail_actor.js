84ac254ace6988d304b9729c4d3ebdb035c835ad
ngonguyenquangtu <20120234@student.hcmus.edu.vn>
2022-10-23 16:26:37 +0700

    Update some feature in actor list display

---
+++ js/detail_actor.js
@@ -0,0 +1,39 @@
+
+import movie_store from "./movie_store.js";
+
+export default {
+    data() {
+        return {
+            query: "Name",
+            movie_store,
+        }
+    },
+    mounted() {
+        //https://imdb-api.com/en/API/Name/k_jro07wuj/nm0000154
+        const url = BASE_URL + this.query + KEY_API + '/' + this.movie_store.id_cur_actor;
+        fetch("https://mocki.io/v1/7a4943b7-6557-4339-8ae4-6656ed3459e2").then(res => res.json().then(data => {
+            console.log(data);
+            this.movie_store.current_actor = data;
+          }))          
+          
+    },
+    methods:{
+    },
+    template:
+    `
+    <div class="movie-container">
+        <div class="card mb-3" style="max-width: 1100px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img :src="this.movie_store.current_actor.image" class="img-fluid rounded-start img-movie" :alt="this.movie_store.current_actor.id">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body text-dark">
+                        <h2 class="card-title">{{this.movie_store.current_actor.name}}</h2>
+                        <p class="card-text">{{this.movie_store.current_actor.summary}}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>`
+}

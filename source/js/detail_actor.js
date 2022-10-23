19bfbe2463a76b10f521bc4bc9d4a7a78575fb01
ngonguyenquangtu <20120234@student.hcmus.edu.vn>
2022-10-23 20:01:54 +0700

    new actions

--- source/js/detail_actor.js
+++ source/js/detail_actor.js
@@ -8,6 +8,11 @@ export default {
             movie_store,
         }
     },
+
+    methods:
+    {
+
+    },
     mounted() {
         //https://imdb-api.com/en/API/Name/k_jro07wuj/nm0000154
         const url = BASE_URL + this.query + KEY_API + '/' + this.movie_store.id_cur_actor;
@@ -16,35 +21,9 @@ export default {
             console.log(data);
             this.movie_store.current_actor = data;
           })) 
+
           $(function(){
-            $('td', 'table').each(function(i) {
-                $(this).text(i+1);
-            });
-            
-            
-            
-            $('table.paginated').each(function() {
-                var currentPage = 0;
-                var numPerPage = 10;
-                var $table = $(this);
-                $table.bind('repaginate', function() {
-                    $table.find('tbody tr').hide().slice(currentPage * numPerPage, (currentPage + 1) * numPerPage).show();
-                });
-                $table.trigger('repaginate');
-                var numRows = $table.find('tbody tr').length;
-                var numPages = Math.ceil(numRows / numPerPage);
-                var $pager = $('<div class="pager"></div>');
-                for (var page = 0; page < numPages; page++) {
-                    $('<span class="page-number"></span>').text(page + 1).bind('click', {
-                        newPage: page
-                    }, function(event) {
-                        currentPage = event.data['newPage'];
-                        $table.trigger('repaginate');
-                        $(this).addClass('active').siblings().removeClass('active');
-                    }).appendTo($pager).addClass('clickable');
-                }
-                $pager.insertBefore($table).find('span.page-number:first').addClass('active');
-            });
+            $('#table_id').DataTable();
           })
     },
     

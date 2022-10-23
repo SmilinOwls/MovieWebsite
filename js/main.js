d1caf3d0528a1f78f7bda9505c441a8948e44b69
ngonguyenquangtu <20120234@student.hcmus.edu.vn>
2022-10-23 16:27:16 +0700

    Add some adjustment for detail movie & actor

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ import footer_nav from "./footer_nav.js"
 import main_body from "./main_body.js"
 import search_view from "./search_view.js"
 import detail_movie from "./detail_movie.js"
+import detail_actor from "./detail_actor.js"
+
 
 export default {
     data() {
@@ -17,7 +19,9 @@ export default {
         footer_nav,
         main_body,
         search_view,
-        detail_movie
+        detail_movie,
+        detail_actor
+
     },
     methods:{
         updateSection(section){
